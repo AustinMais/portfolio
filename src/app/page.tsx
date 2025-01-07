039bb3bff0d234c915494d302f08ec342c8f6a44
Austin Mais <austin.mais@gmail.com>
2025-01-06 17:39:20 -0700

    Update formatting, update bg color, and fixed flex-col issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,54 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <body className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>
-        <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24 gap-1 pl-4 pt-4">
-          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <Link href="/"> <span className="text-[hsl(280,100%,70%)]">Austin</span> Mais</Link>
+    <body className="bg-gradient-to-b from-[#2e026d] to-[#15162c] leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900 lg:flex lg:justify-between lg:gap-4">
+      <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] md:flex-col lg:justify-between lg:py-8 pl-4" >
+        <div>
+          <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+            <Link href="/"><span className="text-[hsl(280,100%,70%)]">Austin</span> Mais</Link>
           </h1>
-          <h2 className="mt-3 text-xl">Full-Stack Software Engineer</h2>
+          <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Full-Stack Software Engineer</h2>
           <p className="mt-4 max-w-xs leading-normal">I design robust applications, modernize architectures, and deliver solutions.</p>
-          <ul className="ml-1 mt-8 flex items-center" aria-label="Social Media">
-            <li className="mr-5 shrink-0 text-xs"> <Link href="https://github.com/AustinMais" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
-            </Link>
+        </div>
+        <nav className="nav hidden lg:block" aria-label="In-page jump links">
+          <ul className="mt-16 w-max">
+            <li>
+              <a className="group flex items-center py-3 active" href="#about">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
+                </span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">About</span>
+              </a>
             </li>
-            <li className="mr-5 shrink-0 text-xs">
-              <Link href="https://www.linkedin.com/in/austin-mais" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path></svg>
-              </Link>
+            <li>
+              <a className="group flex items-center py-3" href="#experience">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
+                </span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Experience</span>
+              </a>
+            </li>
+            <li>
+              <a className="group flex items-center py-3" href="#projects">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
+                </span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Projects</span></a>
             </li>
           </ul>
-        </header>
-      </div>
-      <main>
-        <section id="about" className="container flex flex-col px-4 py-8 gap-1">
+        </nav>
+        <ul className="ml-1 mt-8 flex items-center" aria-label="Social Media">
+          <li className="mr-5 shrink-0 text-xs">
+            <Link href="https://github.com/AustinMais" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+            </Link>
+          </li>
+          <li className="mr-5 shrink-0 text-xs">
+            <Link href="https://www.linkedin.com/in/austin-mais" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path></svg>
+            </Link>
+          </li>
+        </ul>
+      </header>
+      <main id="content" className="pt-24 lg:w-[52%] lg:py-24 md:flex-col">
+        <section id="about" className="container flex md:flex-col px-4 py-8 gap-1">
           <h2 className="text-3xl font-bold">About</h2>
           <p className="mb-4">
             As a seasoned software engineer with over eight years of professional experience, I specialize in designing and developing robust applications and modernizing software architectures. My expertise spans a wide range of technologies, including C#, JavaScript, PHP, React, Angular, Svelte, .NET Framework/Core, Laravel, and cloud platforms like Azure and AWS.
