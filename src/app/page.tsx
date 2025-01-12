4bcc4099e941f019745e0d8ca5a4a82a7c210573
Austin Mais <austin.mais@gmail.com>
2025-01-12 08:47:50 -0700

    Updated formatting and added Portfolio to projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -363,9 +363,57 @@ export default function HomePage() {
           </div>
         </section>
         <section id="projects" className="container flex flex-col px-4 py-8 gap-1">
+          <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2>
+          <div>
+            <ol className="group/list">
+              <li className="mb-12">
+                <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                  <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                  <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                    <img alt="Portfolio Website Card" loading="lazy" decoding="async" data-nimg="1" className="aspect-video object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1 h-50 w-100" src="PortfolioThumbnail.png" data-darkreader-inline-color=""></img>
+                  </header>
+                  <div className="z-10 sm:col-span-6">
+                    <h3 className="font-medium leading-snug text-slate-200">
+                      <div>
+                        <Link className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="https://github.com/AustinMais/portfolio" target="_blank" rel="noreferrer noopener" aria-label="Software Engineer, Bank of America (opens in a new tab)">
+                          <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                          <span>
+                            <span className="inline-block">Portfolio Website
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
+                              </svg>
+                            </span>
+                          </span>
+                        </Link>
+                      </div>
+                    </h3>
+                    <p className="mt-2 text-sm leading-normal">
+                      Developed and designed a portfolio website to showcase projects and resume. Utilized Next.js, Tailwind CSS, and Vercel for deployment.
+                    </p>
+                    <ul className="mt-2 flex flex-wrap" aria-label="Techologies Used">
+                      <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">React</div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">Typescript</div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">HTML&CSS</div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">Tailwind</div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">Vercel</div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+            </ol>
+          </div>
 
         </section>
       </main >
-    </body>
+    </body >
   );
 }
