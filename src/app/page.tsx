f4656fa6e3562728956ada454e2c02a0b4f159f4
Austin Mais <austin.mais@gmail.com>
2026-02-02 17:37:30 -0700

    Updated Technology used in MedSol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,6 +114,12 @@ export default function HomePage() {
                         <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">React</div>
                       </li>
                       <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">Service Fabric</div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">CoPilot</div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
                         <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">SQL</div>
                       </li>
                       <li className="mr-1.5 mt-2">
