180826c3e8f15586c376015b80e75835eb3497f3
Austin Mais <austin.mais@gmail.com>
2026-02-06 17:12:13 -0700

    Update background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ export default {
           DEFAULT: "rgb(52 211 153)",
           muted: "rgb(52 211 153 / 0.15)",
         },
+        chalkboard: {
+          DEFAULT: "#2C5234",
+          dark: "#1a3d28",
+          light: "#36453b",
+        },
       },
     },
   },
